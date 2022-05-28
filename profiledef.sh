b09cbfbffee3483e88347a38da18716564752160
ajwipo <tropaguararia28@gmail.com>
2022-05-28 01:25:24 -0500

    modified linux boot configuration

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="ajlinux"
+iso_label="AJ_$(date +%Y%m)"
+iso_publisher="AJ Linux <https://archlinux.org>"
+iso_application="AJ Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
